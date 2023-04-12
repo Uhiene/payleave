18e638abbe7765a5455e030fb3c13495a66ee85a
Uhiene <princessuhiene@gmail.com>
2023-04-12 23:09:46 +0100

    I corrected the background image in the hero

--- src/views/Mission.jsx
+++ src/views/Mission.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import bg from "../assets/Card.svg";
+import mobileBg from "../assets/Card2.svg"
 import btn1 from "../assets/apple-store-transparent.svg"
 import btn2 from "../assets/play-store-transparent.svg"
 
 const Mission = () => {
+  const imageUrl = window.innerWidth <= 728 ? mobileBg :  bg;
   return (
-    <div className="px-5  py-12  md:p-20">
+    <div className="px-5 py-12 md:p-20">
       <div
-        className="px-5 py-10 md:p-20 flex flex-col items-center text-center space-y-5 rounded-xl bg-no-repeat bg-cover"
-        style={{
-          backgroundImage: `url("${bg}")`,
-        }}
+        className="px-5 py-10 md:p-20 flex flex-col items-center text-center space-y-5 rounded-xl bg-no-repeat bg-cover bg-black  bg-opacity-10"
+        style={{ backgroundImage: `url(${imageUrl})` }}
       >
         <h1 className="text-3xl text-white">Our mission</h1>
         <p className="text-base md:text-xl text-[#D6D6D6]">
